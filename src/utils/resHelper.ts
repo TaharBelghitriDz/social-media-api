1ee82d75c4a982efaf5cc34a7f1cd68fd59e4796
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-09 00:22:15 +0100

    response helper

---
+++ src/utils/resHelper.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+
+export default (res: Response) => ({
+  ...res,
+  done(str: string, args?: { status?: number; resBody?: object }) {
+    return res.status(args?.status || 200).json(args?.resBody || { data: str });
+  },
+  bad(str: string, args?: { status?: number; resBody?: object }) {
+    return res.status(args?.status || 400).json(args?.resBody || { err: str });
+  },
+});
