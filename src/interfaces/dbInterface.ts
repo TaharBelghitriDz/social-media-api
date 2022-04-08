4a34fe77506770a592a69045227d93ee9e075e7b
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-08 18:39:23 +0100

    add model types

--- src/interfaces/dbInterface.ts
+++ src/interfaces/dbInterface.ts
@@ -1,11 +1,17 @@
-export interface userInterface {
+import { Model, Document } from "mongoose";
+
+export interface userInterface extends Document {
   firstName: string;
   lastName: string;
+  password: string;
   email: string;
-  phoneNumber: string;
-  bio: string;
-  cover: string;
-  follwing: { followId: string }[];
-  followers: { followId: string }[];
-  posts: { postId: string }[];
+  phoneNumber?: string;
+  bio?: string;
+  cover?: string;
+  follwing?: { followId: string }[];
+  followers?: { followId: string }[];
+  posts?: { postId: string }[];
+}
+export interface userInterfaceModel extends Model<userInterface> {
+  AddUser: (args: userInterface, clb: (err: any) => void) => void;
 }
