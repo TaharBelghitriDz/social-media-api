3868b9b5c49c9177d05926e1b673d241b811021a
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-09 00:21:49 +0100

    email validation

---
+++ src/utils/validation.ts
@@ -0,0 +1,5 @@
+export const validateEmail = (email: string): boolean => {
+  const regularExpression =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regularExpression.test(String(email).toLowerCase());
+};
