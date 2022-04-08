9e4862eff8cb4321cdb366431b8e51da7e23eaa0
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-08 19:12:13 +0100

    error handler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
-
+import { notFoundError, reqErrHandler } from "./middlewares/reqErorHandler";
 dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(helmet());
 
 const PORT = process.env.PORT || 5000;
+
+app.use(notFoundError);
+app.use(reqErrHandler);
 
 app.listen(PORT, () => {
   console.log("server running on port " + PORT);
