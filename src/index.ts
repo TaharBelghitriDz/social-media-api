99946a95b1d12c2cfac78daaecbcc2fa874b4499
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-08 06:24:16 +0100

    initial the server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/.env" });
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: 100 }));
 app.use(cors());
 app.use(helmet());
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("server running ");
+  console.log("server running on port " + PORT);
 });
