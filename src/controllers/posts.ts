ed354f31b30cb3e39890ccdb2485be4a11a0563c
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-10 00:20:06 +0100

    Update posts.ts

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -70,10 +70,79 @@ export const findPost: RequestHandler = (req, resF) => {
 
 // comments
 
-export const addComment: RequestHandler = (req, resF) => {};
-export const editComment: RequestHandler = (req, resF) => {};
-export const removeCommnet: RequestHandler = (req, resF) => {};
-export const findCommnets: RequestHandler = (req, resF) => {};
+export const addComment: RequestHandler = (req, resF) => {
+  const res = resHelper(resF);
+  const body: { content: string; postId: string } = req.body;
+
+  postDb
+    .findPost([{ _id: body.postId }])
+    .then((post) => {
+      if (!post) throw { err: "we couldn't post comment" };
+
+      post.coments.push({
+        userId: res.locals.user._id,
+        content: body.content,
+        likes: [],
+      });
+      post.save((err) => {
+        if (err) throw { err: "somthing wrong while posting " };
+        res.done("posted");
+      });
+    })
+    .catch((err) => {
+      if (err.err) res.bad(err.err);
+      else res.bad("somthing wrong happned pleas try again");
+    });
+};
+
+export const editComment: RequestHandler = (req, resF) => {
+  const res = resHelper(resF);
+  const body: { content: string; postId: string } = req.body;
+  const id = req.headers.id;
+
+  if (id) {
+    const editQuery = {
+      userId: res.locals.user._id,
+      postId: body.postId,
+      newContent: body.content,
+    };
+    postDb.editComment(editQuery, (err, data) => {
+      if (err) res.bad("somthing wrong happned ");
+      else if (!data) res.bad("unvalid commnet");
+      else res.done("posted");
+    });
+  } else res.bad("somthing went wrong");
+};
+
+export const removeCommnet: RequestHandler = (req, resF) => {
+  const res = resHelper(resF);
+  const postId = req.query.postid;
+  const commentId = req.query.commentid;
+
+  if (!postId && !commentId)
+    res.bad("somthing went wrong pleas refresh the page");
+  else {
+    const removeQuery: any = {
+      userId: `${res.locals.user._id}`,
+      postId,
+      commentId,
+    };
+
+    postDb.removeCommnet(removeQuery, (err) => {
+      if (err) res.bad("somthing wrong happend");
+      else res.done("romeved");
+    });
+  }
+};
+
+export const findCommnets: RequestHandler = (req, resF, next) => {
+  const res = resHelper(resF);
+  const postId = req.query.postid;
+  const range = req.query.range;
+  if (postId && range) {
+    postDb.find({ _id: res.locals.user._id });
+  } else next();
+};
 
 export const addLike: RequestHandler = (req, resF) => {};
 export const updateLike: RequestHandler = (req, resF) => {};
