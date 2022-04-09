f3f867d93fd75ebed896b582f0a9700f9ca952b3
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-09 02:43:09 +0100

    Update index.ts

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,15 @@
 import { RequestHandler } from "express";
+import Ajv from "ajv";
+
+const ajv = new Ajv();
 
 const schemaValidation =
   (schema: object): RequestHandler =>
   (req, res, next) => {
-    next();
+    const validate = ajv.compile(schema);
+    const checkReq = validate(req.body);
+    if (checkReq) next();
+    else res.status(400).json({ err: "unvalid request" });
   };
 
 export default schemaValidation;
