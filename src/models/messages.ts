6b47254e563e771cc34a240497292779a9d4ef00
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-11 02:13:30 +0100

    edit message schema

--- src/models/messages.ts
+++ src/models/messages.ts
@@ -4,25 +4,38 @@ import {
 } from "../interfaces/messageInterface";
 import { Schema, model } from "mongoose";
 import { userDb } from "./user";
+import { userInterface } from "../interfaces/dbInterface";
 
 // you made a big mistake on db structure
 
 const messageSchema = new Schema<messageInterface>({
-  parent: {
-    type: String,
-    default: "",
+  userId: String,
+  otherId: String,
+  seen: {
+    type: Boolean,
+    default: false,
   },
-  content: String,
-  date: String,
-  seen: Boolean,
-  pic: {
-    type: String,
-    default: "",
-  },
-  reaction: {
-    type: String,
-    default: "",
-  },
+  chat: [
+    {
+      parent: {
+        type: String,
+        default: "",
+      },
+      content: String,
+      date: {
+        type: String,
+        default: `${Date}`,
+      },
+      pic: {
+        type: String,
+        default: "",
+      },
+      reaction: {
+        type: String,
+        default: "",
+      },
+    },
+  ],
 });
 
 messageSchema.statics.sendMessage = (
@@ -30,33 +43,42 @@ messageSchema.statics.sendMessage = (
   _id: string,
   otherId: string
 ) =>
-  new Promise<any>((resolve) => {
-    resolve("");
-  })
-    .then(() =>
-      userDb.FindUser({ _id: otherId }, (err, data) => {
-        if (err || !data) throw { err: "unvalid user" };
-        return;
-      })
-    )
+  userDb
+    .find({ $or: [{ _id: otherId }, { _id: _id }] })
+    .then((data) => {
+      if (!data) throw { err: "unvalid user #1" };
+      if (data.length !== 2) throw { err: "unvalid user #2" };
+      return;
+    })
     .then(() => {
       messageDb.findOne(
         {
           $or: [
-            { _id, otherId },
-            { otherId: _id, _id: otherId },
+            { otherId, userId: _id },
+            { userId: otherId, otherId: _id },
           ],
         },
-        (err: any, data: any) => {
-          if (err) throw { err: "unvalid user" };
-          const clb = (err) => {
-            if (err) throw { err: "somthing wrong happend" };
-            return "sent";
-          };
-          if (!data) new messageDb({ content }).save(clb);
-          data.
+        (err: any, data: messageInterface | null) => {
+          if (err) throw { err: "unvalid user #3" };
+          if (!data) return;
+          return data;
         }
       );
+    })
+    .then((data: any) => {
+      if (data) {
+        data.chat.push({ content });
+        data.save((err: any) => {
+          if (err) throw { err: "somthing wrong happend" };
+          return;
+        });
+      }
+      const newChat: any = new messageDb({ userId: _id, otherId });
+      newChat.chat.push({ content });
+      newChat.save((err: any) => {
+        if (err) throw { err: "somthing wrong happend" };
+        return;
+      });
     });
 
 export const messageDb = model<messageInterface, messageModelInterface>(
