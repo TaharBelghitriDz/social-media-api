0855b03ee799e3478d97e59df82c44c311f6cd67
TaharBelghitriDz <tahartlm0@gmail.com>
2022-04-12 03:33:34 +0100

    Update message.ts

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -15,6 +15,59 @@ const sendMessage: JSONSchemaType<{
   additionalProperties: true,
 };
 
-const messagesSchemas: any = { sendMessage };
+const reaction: JSONSchemaType<{
+  reaction: string;
+  otherId: string;
+  messageId: string;
+}> = {
+  type: "object",
+  properties: {
+    reaction: { type: "string", maxLength: 10 },
+    otherId: { type: "string", maxLength: 100 },
+    messageId: { type: "string", maxLength: 100 },
+  },
+  required: ["reaction", "otherId", "messageId"],
+  additionalProperties: true,
+};
+
+const removeMessage: JSONSchemaType<{
+  otherId: string;
+  messageId: string;
+}> = {
+  type: "object",
+  properties: {
+    otherId: { type: "string", maxLength: 100 },
+    messageId: { type: "string", maxLength: 100 },
+  },
+  required: ["otherId", "messageId"],
+  additionalProperties: true,
+};
+
+const getMessages: JSONSchemaType<{ range: number }> = {
+  type: "object",
+  properties: {
+    range: { type: "number" },
+  },
+  required: ["range"],
+  additionalProperties: true,
+};
+
+const findChats: JSONSchemaType<{ range: number; chatId: string }> = {
+  type: "object",
+  properties: {
+    range: { type: "number" },
+    chatId: { type: "string", maxLength: 100 },
+  },
+  required: ["range", "chatId"],
+  additionalProperties: true,
+};
+
+const messagesSchemas: any = {
+  sendMessage,
+  reaction,
+  removeMessage,
+  getMessages,
+  findChats,
+};
 
 export default messagesSchemas;
